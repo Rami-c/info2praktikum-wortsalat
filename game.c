8b8e2147ca9cbda7c824d14fefe38e2c00764964
Rami <faressadikra91014@th-nuernberg.de>
2025-10-19 20:17:39 +0200

    testen

--- game.c
+++ game.c
@@ -2,6 +2,7 @@
 #include <time.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 #define MAX_RAND_TRIES_PER_WORD 10
 #define EMPTY_CHAR 0
@@ -13,7 +14,7 @@
 // Creates the word salad by placing words randomly and filling empty spaces
 int createWordSalad(char salad[MAX_SEARCH_FIELD_LEN][MAX_SEARCH_FIELD_LEN], unsigned int searchFieldLen, const char words[][MAX_WORD_LEN], unsigned int wordCount)
 {
-
+   printf("rami\n");
 }
 
 // Prints the word salad to console
